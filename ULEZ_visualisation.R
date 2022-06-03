d6e22c625ec936accc4fddaa6a30a10b7f2d79b7
craigpoku <65303787+craigpoku@users.noreply.github.com>
2022-06-03 23:08:34 +0100

    Update ULEZ_visualisation.R

--- ULEZ_visualisation.R
+++ ULEZ_visualisation.R
@@ -21,103 +21,59 @@ ULEZ_compliance_df = read.csv("~/Github/live_change_point_development/ULEZVehicl
 
 cp_urban_background_no2_df_all = normalised_urban_background_no2_all_sites_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean) %>%
+  select(date, d7_rollavg_normal_mean)%>%
+  mutate(df_header = "Urban background NO2") %>%
   rename(value = d7_rollavg_normal_mean)
 
 cp_roadside_no2_df_all = normalised_roadside_no2_all_sites_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean) %>%
+  select(date, d7_rollavg_normal_mean)%>%
+  mutate(df_header = "Roadside NO2") %>%
   rename(value = d7_rollavg_normal_mean)
 
 cp_roadside_pm25_df_ULEZ = normalised_roadside_pm25_ULEZ_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean) %>%
+  select(date, d7_rollavg_normal_mean)%>%
+  mutate(df_header = "Roadside PM2.5 ULEZ") %>%
   rename(value = d7_rollavg_normal_mean)
 
 cp_roadside_no2_df_ULEZ = normalised_roadside_no2_ULEZ_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean) %>%
+  select(date, d7_rollavg_normal_mean)%>%
+  mutate(df_header = "Roadside NO2 ULEZ") %>%
   rename(value = d7_rollavg_normal_mean)
 
 compliance_statistics_cp_df = ULEZ_compliance_df  %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
   select(date, d7_rollavg_compliant_percentage) %>%
-  rename(value = d7_rollavg_compliant_percentage) 
+  mutate(df_header = "Compiance stats ULEZ") %>%
+  rename(value = d7_rollavg_compliant_percentage)
 
+ULEZ_total_cp_df = rbind(cp_roadside_no2_df_ULEZ,
+                         cp_roadside_pm25_df_ULEZ,
+                         compliance_statistics_cp_df)
 
+ULEZ_total_cp_code = unique(as.character(ULEZ_total_cp_df$df_header))
 
-urban_background_no2_cp_detection = map_dfr(.x = win_length_vector,
-                             .f = ~window_length_constrain(df = cp_urban_background_no2_df_all,
-                                                           .x, cp_factor = 2.5,
+ULEZ_example_detected_cps = map2_dfr(.x = 7, .y = ULEZ_total_cp_code,
+                  .f = ~multi_var_ts_gradient_cp_detection(df = ULEZ_total_cp_df,
+                                                           .x, .y, cp_factor = 2,
                                                            epsilon = 1e-9, date = TRUE))
 
-roadside_no2_cp_detection_all_sites = map_dfr(.x = win_length_vector,
-                                            .f = ~window_length_constrain(df = cp_roadside_no2_df_all,
-                                                                          .x, cp_factor = 2.5,
-                                                                          epsilon = 1e-9, date = TRUE))
-
-roadside_no2_cp_detection_ULEZ = map_dfr(.x = win_length_vector,
-                                              .f = ~window_length_constrain(df = cp_roadside_no2_df_ULEZ,
-                                                                            .x, cp_factor = 2,
-                                                                            epsilon = 1e-9, date = TRUE))
-roadside_pm25_cp_detection_ULEZ = map_dfr(.x = win_length_vector,
-                                         .f = ~window_length_constrain(df = cp_roadside_pm25_df_ULEZ,
-                                                                       .x, cp_factor = 2,
-                                                                       epsilon = 1e-9, date = TRUE))
-
-compliance_statistics_cp_detection_ULEZ = map_dfr(.x = 6,
-                                          .f = ~window_length_constrain(df = compliance_statistics_cp_df,
-                                                                        .x, cp_factor = 2,
-                                                                        epsilon = 0, date = TRUE))
-
-compliance_statistics_cp_detection_ULEZ_plot = compliance_statistics_cp_detection_ULEZ %>%
-  filter(date >= as.Date("2019-03-15"), variables=="Test dataset")%>%
-  ggplot(aes(x = date, y = value))+
-  annotate("rect", xmin = as.Date("2019-04-08"), 
-           xmax = as.Date("2019-06-30"), ymin = -Inf, ymax = Inf, 
+ULEZ_example_detected_cps %>%
+  filter(date >= as.Date("2019-03-01") & date <= as.Date("2019-06-30"), 
+         variables == "Input dataset") %>% 
+  ggplot(aes(x = date, y = value)) + 
+  geom_line(aes(colour=df_label), lwd = 1.5)+
+  annotate("rect", xmin = as.POSIXct("2019-04-08"), 
+           xmax = as.POSIXct("2019-06-30"), ymin = -Inf, ymax = Inf, 
            alpha = .2)+
-  geom_line(colour = "red", lwd = 1.5)+
-  geom_point(data = filter(compliance_statistics_cp_detection_ULEZ,
-                           cp==TRUE & date >= as.Date("2019-03-15") & variables %in% c("Test dataset"))
+  geom_point(data = filter(ULEZ_example_detected_cps,
+                           cp==TRUE & date >= as.POSIXct("2019-03-15") & variables %in% c("Input dataset"))
              , size  = 3, colour = "blue")+
-  labs(x= "Date")+
-  coord_cartesian(xlim = c(as.Date("2019-03-15"), as.Date("2019-06-30")))+
-  ylab(quickText("Rolling 7 day ULEZ vehicle compliance (%)")) 
+  labs(x= "Date", y = "Rolling 7 day Various Units", colour = "Human impact comparison")+
+  facet_grid(df_label~., scales = "free_y")+
+  ggtitle("Applied CPD example - ULEZ AQ vs compliance")
 
-
-roadside_pm25_cp_detection_ULEZ_plot = roadside_pm25_cp_detection_ULEZ %>%
-  filter(date >= as.Date("2019-03-15"), variables=="Test dataset")%>%
-  ggplot(aes(x = date, y = value))+
-  annotate("rect", xmin = as.POSIXct(as.Date("2019-04-08")), 
-           xmax = as.POSIXct(as.Date("2019-06-30")), ymin = -Inf, ymax = Inf, 
-           alpha = .2)+
-  geom_line(colour = "orange", lwd = 1.5)+
-  geom_point(data = filter(roadside_pm25_cp_detection_ULEZ,
-                           cp==TRUE & date >= as.Date("2019-03-15") & variables %in% c("Test dataset"))
-             , size  = 3, colour = "blue")+
-  labs(x= "Date")+
-  ylab(quickText("Rolling 7 day normal pm25 (ug/m3)")) +
-  geom_label(aes(x = as.POSIXct(as.Date("2019-06-01")), y = 17, label = "ULEZ implementation"), 
-             fill = "white",
-             size = 6)+ggtitle("Applied CPD example - ULEZ AQ vs compliance")+ 
-  theme(plot.title = element_text(size = 20, face = "bold"))
-
-roadside_no2_cp_detection_ULEZ_plot = roadside_no2_cp_detection_ULEZ %>%
-  filter(date >= as.Date("2019-03-15"), variables=="Test dataset")%>%
-  ggplot(aes(x = date, y = value))+
-  annotate("rect", xmin = as.POSIXct(as.Date("2019-04-08")), 
-           xmax = as.POSIXct(as.Date("2019-06-30")), ymin = -Inf, ymax = Inf, 
-           alpha = .2)+
-  geom_line(colour = "green", lwd = 1.5)+
-  geom_point(data = filter(roadside_no2_cp_detection_ULEZ,
-                           cp==TRUE & date >= as.Date("2019-03-15") & variables %in% c("Test dataset"))
-             , size  = 3, colour = "blue")+
-  labs(x= "Date") +
-  ylab(quickText("Rolling 7 day normal no2 (ug/m3)"))
-
-roadside_pm25_cp_detection_ULEZ_plot/roadside_no2_cp_detection_ULEZ_plot/compliance_statistics_cp_detection_ULEZ_plot
-
-
-
-
+  
 
